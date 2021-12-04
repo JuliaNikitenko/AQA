aa74214704514444a6afbe5e66d0c0c23a466a46
Yuliia Nikitenko <juliya.nikitenko@gmail.com>
2021-12-04 17:57:43 +0200

    test_2_1

--- src/test/java/Test_1.java
+++ src/test/java/Test_1.java
@@ -1,7 +1,10 @@
 public class Test_1 {
 
-    public static void main(String[] args) {
+    public static void main(String[] args)
+    {
         System.out.println("Hello, JAVA!");
+        System.out.println("Hello, JAVA!");
+
     }
 }
 
