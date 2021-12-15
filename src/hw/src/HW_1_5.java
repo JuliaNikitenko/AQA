083762a83c5f568efe39c1e5f63dc19759c9204c
Yuliia Nikitenko <juliya.nikitenko@gmail.com>
2021-12-16 01:27:12 +0200

    Fours_exercise_

--- src/hw/src/HW_1_5.java
+++ src/hw/src/HW_1_5.java
@@ -1,9 +1,37 @@
 public class HW_1_5 {
     public static void main(String[] args) {
-        String name1 = "SergeY";
-        String name2 = "Sergey";
+        String season;
+        int month = (int) ((Math.random() * 13) + 1); //номер месяца увеличен до 14, чтобы иногда было некорректное значение
+        System.out.println("Месяц: " + month);
 
-        System.out.println(name1.equalsIgnoreCase(name2) ? "Они тезки" : "Они не тезки");
+        switch (month) {
+            case 12:
+            case 1:
+            case 2:
+                season = "зима";
+                break;
+            case 3:
+            case 4:
+            case 5:
+                season = "весна";
+                break;
+            case 6:
+            case 7:
+            case 8:
+                season = "лето";
+                break;
+            case 9:
+            case 10:
+            case 11:
+                season = "осень";
+                break;
+            default:
+                season = "не определен";
+                break;
+        }
+        System.out.println("Сезон: " + season);
+
     }
 
 }
+
