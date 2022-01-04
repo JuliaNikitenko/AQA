780f068d27cab02285a6b81b33b44299a39fcb4a
Yuliia Nikitenko <juliya.nikitenko@gmail.com>
2022-01-04 23:47:44 +0200

    update_1.3

--- src/hw/src/HW_1_3.java
+++ src/hw/src/HW_1_3.java
@@ -1,20 +1,22 @@
 import java.util.Arrays;
 import java.util.Scanner;
 
+
 public class HW_1_3 {
     public static void main(String[] args) {
         Scanner in = new Scanner(System.in);
         System.out.println("Введите 5 чисел");
         int[] arr = new int[5];
-        int numMin = arr[0];
-        int numMax = arr[0];
+        int numMax = 0;
+        int numMin = 0;
+
 
         for (int i = 0; i < arr.length; i++) {
             System.out.print("Введите " + (i + 1) + "-е число: ");
             arr[i] = in.nextInt();
 
-            numMin = Math.min(numMin, arr[i]);
-            numMax = Math.max(numMax, arr[i]);
+            numMin = (i == 0) ? arr[i] : Math.min(numMin, arr[i]);
+            numMax = (i == 0) ? arr[i] : Math.max(numMax, arr[i]);
         }
 
         System.out.println("Введенный массив: " + Arrays.toString(arr));
