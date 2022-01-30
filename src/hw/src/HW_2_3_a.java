4bac2d01e02698616cd6be2332dd8c2e22d0a2d8
Yuliia Nikitenko <juliya.nikitenko@gmail.com>
2022-01-30 22:31:17 +0200

    HW_2_3_a цикл while первые 100 нечетных чисел

---
+++ src/hw/src/HW_2_3_a.java
@@ -0,0 +1,10 @@
+public class HW_2_3_a {
+    public static void main(String[] args) {
+        int i = 1;
+        while (i < 100) {
+            System.out.println(i);
+            i = i + 2;
+        }
+    }
+}
+
