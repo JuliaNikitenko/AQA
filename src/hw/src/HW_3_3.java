775738bbe6af18bb53ac662b567193e5e245aa80
Yuliia Nikitenko <juliya.nikitenko@gmail.com>
2022-02-02 00:16:42 +0200

    HW_3_2 Поиск наименьшего (-их) элемента (-ов) массива размерности n

---
+++ src/hw/src/HW_3_3.java
@@ -0,0 +1,29 @@
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class HW_3_3 {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        System.out.println("Введите размер массива - целое число n: ");
+        int n = input.nextInt();
+        int[] arr = new int[n];
+        System.out.println("Введите n чисел массива: ");
+        for (int i = 0; i < n; i++) {
+            arr[i] = input.nextInt();
+        }
+        System.out.println(Arrays.toString(arr));
+
+        int maxElement = arr[0];
+        int count = 1;
+
+        for (int i = 1; i < arr.length; i++) {
+            if (arr[i] > maxElement) {
+                maxElement = arr[i];
+                count = 1;
+            } else if (maxElement == arr[i]) {
+                count++;
+            }
+
+        }System.out.println("Максимальный элемент " + maxElement + " встречается " + count + " раз(а)");
+    }
+}
